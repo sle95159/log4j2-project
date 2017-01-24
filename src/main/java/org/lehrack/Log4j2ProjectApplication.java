a324e7ba1cd108dcaa88f8ecf3e5f635d37c03da
sle <sle95159@mailbox.org>
2017-01-24 07:28:57 +0100

    sfsa

--- src/main/java/org/lehrack/Log4j2ProjectApplication.java
+++ src/main/java/org/lehrack/Log4j2ProjectApplication.java
@@ -1,13 +1,18 @@
 package org.lehrack;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Log4j2ProjectApplication {
 
+	private final static Logger log = LogManager.getLogger();
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Log4j2ProjectApplication.class, args);
 		System.out.println("Log4J2 says hello ...");
+		log.info("Log4J2 says hello ...");
 	}
 }
